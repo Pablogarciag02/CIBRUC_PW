1845c1aedec5aed93cb857f8bad70b82a6c13598
Pablo Garcia <pgarciag566@gmail.com>
2022-08-22 23:52:55 -0500

    changed background

--- src/Components/Startpage/Header.js
+++ src/Components/Startpage/Header.js
@@ -5,10 +5,10 @@ export default function Header() {
   return (
     <div>
         <div className="flex justify-end mt-14 mr-24  invisible lg:visible">
-            <span className = "mr-8 hover:underline"><a href="mailto:pgarciag566@gmail.com">Inicio</a></span>
-            <span className = "mr-8 hover:underline"><a href="#" target="_blank">Quienes Somos</a></span>
-            <span className = "mr-8 hover:underline"><a href="#" target="_blank">Quiero Ser un Super SER</a></span>
-            <span className = "mr-8 hover:underline"><a href="#" target="_blank">Contacto</a></span>
+            <span className = "mr-8 hover:underline hover:text-darkgreen"><a href="#">Inicio</a></span>
+            <span className = "mr-8 hover:underline hover:text-darkgreen"><a href="#" target="_blank">Quienes Somos</a></span>
+            <span className = "mr-8 hover:underline hover:text-darkgreen"><a href="#" target="_blank">Quiero Ser un Super SER</a></span>
+            <span className = "mr-8 hover:underline hover:text-darkgreen"><a href="#" target="_blank">Contacto</a></span>
         </div>
     </div>
   )
