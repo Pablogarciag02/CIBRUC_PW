5c0eb5b71fc8eb93ebecba0120904e5aa78e2cae
Pablo Garcia <pgarciag566@gmail.com>
2022-08-23 11:21:32 -0500

    header now works and is responsive

--- src/Components/Startpage/Header.js
+++ src/Components/Startpage/Header.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import "./Header.css"
+import IMAGES from '../Images'
 
 export default function Header() {
   return (
-    <div>
-        <div className="flex justify-end mt-14 mr-24  invisible lg:visible">
-            <span className = "mr-8 hover:underline hover:text-darkgreen"><a href="#">Inicio</a></span>
-            <span className = "mr-8 hover:underline hover:text-darkgreen"><a href="#" target="_blank">Quienes Somos</a></span>
-            <span className = "mr-8 hover:underline hover:text-darkgreen"><a href="#" target="_blank">Quiero Ser un Super SER</a></span>
-            <span className = "mr-8 hover:underline hover:text-darkgreen"><a href="#" target="_blank">Contacto</a></span>
-        </div>
+    <div className='flex justify-between'>
+    
+      <img className="h-40 sm:h-48 md:h-64" src={IMAGES.myLogo} alt=""></img>
+  
+
+      <div className="mt-14 mr-24  invisible lg:visible">
+        <span className = "mr-8 hover:underline hover:text-darkgreen"><a href="#">Inicio</a></span>
+        <span className = "mr-8 hover:underline hover:text-darkgreen"><a href="#" target="_blank">Quienes Somos</a></span>
+        <span className = "mr-8 hover:underline hover:text-darkgreen"><a href="#" target="_blank">Quiero Ser un Super SER</a></span>
+        <span className = "mr-8 hover:underline hover:text-darkgreen"><a href="#" target="_blank">Contacto</a></span>
+      </div>
     </div>
   )
 }
