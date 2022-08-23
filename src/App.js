f0611358710576baf680cac964002dceba7a76fe
Pablo Garcia <pgarciag566@gmail.com>
2022-08-23 11:20:46 -0500

    added modal component (nonfunctional)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import Header from './Components/Startpage/Header';
 import "./App.css"
+import Header from './Components/Startpage/Header';
+import ModalBtn from "./Components/Startpage/Phoneview/ModalBtn";
+import Startbody from "./Components/Startpage/Startbody";
+
+
 
 function App() {
   return (
     <>
       <Header />
+      <ModalBtn />
+      <Startbody />
     </>
       
     
---
+++ src/Components/Startpage/Phoneview/ModalBtn.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+
+export default function ModalBtn() {
+  return (
+    <div className ="float-right mr-8 lg:invisible ">
+        <button id = "square"
+            className ="" href="">
+            <div id = "burgerwrap" className="">
+                <span></span>
+            
+                <span></span>
+            </div>
+        </button>
+    </div> 
+  )
+}
+
