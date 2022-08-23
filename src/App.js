f57fe3f191500daf6d7c3330e757c214a3885f35
Pablo Garcia <pgarciag566@gmail.com>
2022-08-22 22:42:06 -0500

    added app components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './Components/Startpage/Header';
+import "./App.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <>
+      <Header />
+    </>
       
-        <p className='bg-red-400'>prueba</p>
-        
-      </header>
-    </div>
+    
   );
 }
 
