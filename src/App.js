50da7e3387a69b523db15e33efc462a18f794ea8
Pablo Garcia <pgarciag566@gmail.com>
2022-09-07 16:51:52 -0500

    added routes to app

--- src/App.js
+++ src/App.js
@@ -1,39 +1,27 @@
 import "./App.css"
+import React from 'react'
 import { useState, useEffect } from "react";
 import Header from './Components/Startpage/Header';
 import ModalBtn from "./Components/Startpage/Phoneview/ModalBtn";
 import Startbody from "./Components/Startpage/Startbody";
 import SuperSER from "./Components/Startpage/SuperSER";
+import Home from "./Components/Views/Home";
 
+import { Routes, Route, useLocation } from "react-router-dom";
+import Quienessomos from "./Components/Views/Quienessomos";
+import QuieroSER from "./Components/Views/QuieroSER";
+import Contacto from "./Components/Views/Contacto";
 
-
-
-function App() {
-  const [isDesktop, setDesktop] = useState(window.innerWidth > 650);
-
-  const updateMedia = () => {
-    setDesktop(window.innerWidth > 650)
-  };
-
-  useEffect (() => {
-    window.addEventListener("resize", updateMedia);
-    return () => window.removeEventListener("resize", updateMedia);
-  })
+export default function App() {
+  const location = useLocation();
+  const currentPath = location.pathname
   return (
-    <>
-      {isDesktop ? (
-        <Header />
-      ) : (
-        <ModalBtn />
-      )}
-
-      <Startbody />
-      <SuperSER />
-
-    </>
+    <Routes>
       
-    
-  );
+      <Route path ="/" element = { <Home />}/>
+      <Route path ="/QuienesSomos" element = { <Quienessomos />} />
+      <Route path ="/QuieroSER" element = {<QuieroSER />} />
+      <Route path = "/Contacto" element = {<Contacto />} />
+    </Routes>
+  )
 }
-
-export default App;
