007f04d04ce4fa315ab8bee8218c60626d534520
Pablo Garcia <pgarciag566@gmail.com>
2022-09-07 17:52:11 -0500

    changed home route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import "./App.css"
 import React from 'react'
-import { useState, useEffect } from "react";
-import Header from './Components/Startpage/Header';
-import ModalBtn from "./Components/Startpage/Phoneview/ModalBtn";
-import Startbody from "./Components/Startpage/Startbody";
-import SuperSER from "./Components/Startpage/SuperSER";
 import Home from "./Components/Views/Home";
 
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, Router } from "react-router-dom";
 import Quienessomos from "./Components/Views/Quienessomos";
 import QuieroSER from "./Components/Views/QuieroSER";
 import Contacto from "./Components/Views/Contacto";
 
 export default function App() {
-  const location = useLocation();
-  const currentPath = location.pathname
   return (
-    <Routes basename={process.env.PUBLIC_URL}>
+    <Routes>
       
-      <Route path ="/" element = { <Home />}/>
+      <Route path ="/CIBRUC_PW" element = { <Home />}/>
       <Route path ="/QuienesSomos" element = { <Quienessomos />} />
       <Route path ="/QuieroSER" element = {<QuieroSER />} />
       <Route path = "/Contacto" element = {<Contacto />} />
--- src/Components/Startpage/Header.js
+++ src/Components/Startpage/Header.js
@@ -13,7 +13,7 @@ export default function Header() {
       <img className="mt-5 ml-5 h-40 "  src={IMAGES.myLogo}  alt=""></img>
   
       <div className="mt-14 mr-24  invisible lg:visible">
-        <span className = "mr-8 text-xl hover:underline hover:text-darkgreen"><a href="/">Inicio</a></span>
+        <span className = "mr-8 text-xl hover:underline hover:text-darkgreen"><a href="/CIBRUC_PW">Inicio</a></span>
         <span className = "mr-8 text-xl hover:underline hover:text-darkgreen"><a href="/QuienesSomos">Quienes Somos</a></span>
         <span className = "mr-8 text-xl hover:underline hover:text-darkgreen"><a href="/QuieroSER">Quiero Ser un Super SER</a></span>
         <span className = "mr-8 text-xl hover:underline hover:text-darkgreen"><a href="/Contacto">Contacto</a></span>
