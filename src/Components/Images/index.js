c9c9a8d9288e6854b5c6a732376e0a0d05cdbb8f
Pablo Garcia <pgarciag566@gmail.com>
2022-09-07 16:51:17 -0500

    Added Images

--- src/Components/Images/index.js
+++ src/Components/Images/index.js
@@ -1,7 +1,14 @@
 const IMAGES = {
     myLogo: require("./Image-Logo1.png"),
     recycleIcon: require("./Recycle-icon.png"),
-    goBack: require("./Goback.png")
+    goBack: require("./Goback.png"),
+    mainLogo: require("./Logo.png"),
+
+    instagram: require("./003-instagram-1.png"),
+    twitter: require("./002-twitter.png"),
+    tiktok: require("./004-tik-tok.png"),
+    facebook: require("./006-facebook.png")
+
 }
 
 export default IMAGES
