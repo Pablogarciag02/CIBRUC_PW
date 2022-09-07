95025836fc971db3ebf2e3466a5f2a19be8277eb
Pablo Garcia <pgarciag566@gmail.com>
2022-09-06 22:47:54 -0500

    made prettier

--- src/Components/Startpage/Header.js
+++ src/Components/Startpage/Header.js
@@ -6,14 +6,14 @@ export default function Header() {
   return (
     <div className='flex justify-between'>
     
-      <img className="h-40 sm:h-48 md:h-64" src={IMAGES.myLogo} alt=""></img>
+      <img className="mt-5 ml-5 h-40 "  src={IMAGES.myLogo}  alt=""></img>
   
 
       <div className="mt-14 mr-24  invisible lg:visible">
-        <span className = "mr-8 hover:underline hover:text-darkgreen"><a href="#">Inicio</a></span>
-        <span className = "mr-8 hover:underline hover:text-darkgreen"><a href="#" target="_blank">Quienes Somos</a></span>
-        <span className = "mr-8 hover:underline hover:text-darkgreen"><a href="#" target="_blank">Quiero Ser un Super SER</a></span>
-        <span className = "mr-8 hover:underline hover:text-darkgreen"><a href="#" target="_blank">Contacto</a></span>
+        <span className = "mr-8 text-xl hover:underline hover:text-darkgreen"><a href="#">Inicio</a></span>
+        <span className = "mr-8 text-xl hover:underline hover:text-darkgreen"><a href="#" target="_blank">Quienes Somos</a></span>
+        <span className = "mr-8 text-xl hover:underline hover:text-darkgreen"><a href="#" target="_blank">Quiero Ser un Super SER</a></span>
+        <span className = "mr-8 text-xl hover:underline hover:text-darkgreen"><a href="#" target="_blank">Contacto</a></span>
       </div>
     </div>
   )
