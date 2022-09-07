a1be959176fcb07f1b8973428c4c4bba6bb5b33d
Pablo Garcia <pgarciag566@gmail.com>
2022-09-07 16:52:59 -0500

    header component, (might remove)

---
+++ src/Components/Startpage/Inicio.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Inicio() {
+  return (
+    <span className = "mr-8 text-xl hover:underline hover:text-darkgreen"><a href="/">Inicio</a></span>
+  )
+}
