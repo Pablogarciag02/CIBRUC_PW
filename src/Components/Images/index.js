dd4b6cf1f9a0930f86a5e578f2076589a3ae7417
Pablo Garcia <pgarciag566@gmail.com>
2022-09-06 22:47:38 -0500

    added images

--- src/Components/Images/index.js
+++ src/Components/Images/index.js
@@ -1,5 +1,7 @@
 const IMAGES = {
-    myLogo: require("./Image-Logo1.png")
+    myLogo: require("./Image-Logo1.png"),
+    recycleIcon: require("./Recycle-icon.png"),
+    goBack: require("./Goback.png")
 }
 
 export default IMAGES
