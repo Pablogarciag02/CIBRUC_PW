7e1d37f4b0d43e9ceccad029ad4c1cb05a250586
Pablo Garcia <pgarciag566@gmail.com>
2022-09-07 16:52:33 -0500

    added different headers depending on route

--- src/Components/Startpage/HeaderInicio.js
+++ src/Components/Startpage/HeaderInicio.js
@@ -1,19 +1,16 @@
 import React from 'react'
 import "./Header.css"
 import IMAGES from '../Images'
-import {useLocation } from "react-router-dom"
 
 
-export default function Header() {
-  const location = useLocation();
-  const currentPath = location.pathname
+export default function HeaderInicio() {
   return (
     <div className='flex justify-between'>
     
       <img className="mt-5 ml-5 h-40 "  src={IMAGES.myLogo}  alt=""></img>
   
+
       <div className="mt-14 mr-24  invisible lg:visible">
-        <span className = "mr-8 text-xl hover:underline hover:text-darkgreen"><a href="/">Inicio</a></span>
         <span className = "mr-8 text-xl hover:underline hover:text-darkgreen"><a href="/QuienesSomos">Quienes Somos</a></span>
         <span className = "mr-8 text-xl hover:underline hover:text-darkgreen"><a href="/QuieroSER">Quiero Ser un Super SER</a></span>
         <span className = "mr-8 text-xl hover:underline hover:text-darkgreen"><a href="/Contacto">Contacto</a></span>
