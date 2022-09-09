1e8d53559745aa081c97a561e0c88b7d7c63791b
Pablo Garcia <pgarciag566@gmail.com>
2022-09-08 19:39:19 -0500

    added hrefs to main page

--- src/Components/Startpage/Header.js
+++ src/Components/Startpage/Header.js
@@ -1,20 +1,19 @@
 import React from 'react'
 import "./Header.css"
+
 import IMAGES from '../Images'
-import {useLocation } from "react-router-dom"
+
 
 
 export default function Header() {
-  const location = useLocation();
-  const currentPath = location.pathname
   return (
-    <div className='flex justify-between'>
+    <div id="headerview" className='flex justify-between'>
     
       <img className="mt-5 ml-5 h-40 "  src={IMAGES.mainLogo}  alt=""></img>
   
       <div className="mt-14 mr-24  invisible lg:visible">
-        <span className = "mr-8 text-xl hover:underline hover:text-darkgreen"><a href="/CIBRUC_PW">Inicio</a></span>
-        <span className = "mr-8 text-xl hover:underline hover:text-darkgreen"><a href="/CIBRUC_PW/QuienesSomos">Quienes Somos</a></span>
+        <span className = "mr-8 text-xl hover:underline hover:text-darkgreen"><a href="#headerview">Inicio</a></span>
+        <span className = "mr-8 text-xl hover:underline hover:text-darkgreen"><a href="#about">Quienes Somos</a></span>
         <span className = "mr-8 text-xl hover:underline hover:text-darkgreen"><a href="/CIBRUC_PW/QuieroSER">Quiero Ser un Super SER</a></span>
         <span className = "mr-8 text-xl hover:underline hover:text-darkgreen"><a href="/CIBRUC_PW/Contacto">Contacto</a></span>
       </div>
