9dd4c2b62a2658826717a94734e73e0bcdbb4172
Pablo Garcia <pgarciag566@gmail.com>
2022-09-27 22:26:30 -0500

    removed li list

--- src/Components/Quienes Somos/CumplicionMeta.js
+++ src/Components/Quienes Somos/CumplicionMeta.js
@@ -1,30 +1,17 @@
 import React from 'react'
 import IMAGES from '../Images'
+import Qslider from '../sliderHacemos/Qslider'
 
 export default function CumplicionMeta() {
   return (
-    <div className='m-10 mb-80 '>
+    <div className='m-10'>
         <div className='m-10'>
             <div className='justify-center sm:flex'>
                 <div>
-                    <h1 className='flex items-center mb-2 text-3xl sm:underline sm:mx-40 sm:underline-offset-8'>Como Logramos esa Meta</h1>
-                    <h1 className='flex items-center text-xl sm:mx-40 sm:text-1xl'>En CIBRUC estamos comprometidos en realizar cambio a traves de...</h1>
-                    
-                    <div id="listAbout" className='items-start grid-cols-2 sm:grid sm:ml-40'>
-                    <ul className='list-disc'>
-                        <li className='mt-4 sm:mr-5 sm:mt-4'>Talleres y Cursos de Capacitación</li>
-                        <li className='mt-4 sm:mr-5 sm:mt-8'>Estudios de caracterización de Residuos</li>
-                        <li className='mt-4 sm:mr-5 sm:mt-4'>Investigaciones Medioambientales</li>
-                        <li className='mt-4 sm:mr-5 sm:mt-4'>Proyectos Ejecutivos de RSU</li>
-                        <li className='mt-4 sm:mr-5 sm:mt-4'>Revisión y Opiniones técnicas de Technologías de Tratamientos de RSU</li>
-                    </ul>
-
-                    <ul className='list-disc'>
-                        <li className='mt-4 sm:mt-4 sm:ml-1'>Implementación de Proyectos de Biodigestión de Proyectos de Biodigestión Anaerobia</li>
-                        <li className='mt-4 sm:mt-2 sm:ml-1'>Implementación de Proyectos de Composta y Fertilizante</li>
-                        <li className='mt-4 sm:mt-4 sm:ml-1'>Implementación de Proyectos de Combustible Derivado de Residuos</li>
-                        <li className='mt-4 sm:mt-8 sm:ml-1'>Asesoría a Municipios, Entidades Federativas y funcionarios gubernamentales.</li>
-                    </ul>
+                    <h1 className='flex items-center mb-2 text-6xl sm:ml-40 '>Como Logramos esa Meta</h1>
+                    <h1 className='flex items-center text-xl sm:ml-40 sm:text-1xl'>En CIBRUC estamos comprometidos en realizar cambio a traves de...</h1>
+                    <div className='border-black sm:mx-40'>
+                        <Qslider />
                     </div>
                 </div>
                 <img className='sm:w-1/2' src={IMAGES.goalsAbout}></img>
