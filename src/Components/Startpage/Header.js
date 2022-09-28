8b39bc429694bb818fac0ea98937de6f9d4ed1a9
Pablo Garcia <pgarciag566@gmail.com>
2022-09-27 22:27:50 -0500

    added margin

--- src/Components/Startpage/Header.js
+++ src/Components/Startpage/Header.js
@@ -9,13 +9,13 @@ export default function Header() {
   return (
     <div id="headerview" className='flex justify-between'>
     
-      <img className="mt-5 ml-5 h-40 "  src={IMAGES.mainLogo}  alt=""></img>
+      <img className="h-40 mt-5 ml-5 "  src={IMAGES.mainLogo}  alt=""></img>
   
-      <div className="mt-14 mr-24  invisible lg:visible">
+      <div className="invisible mr-24 mt-14 lg:visible">
         <span className = "mr-8 text-xl hover:underline hover:text-darkgreen"><a href="#headerview">Inicio</a></span>
-        <span className = "mr-8 text-xl hover:underline hover:text-darkgreen"><a href="#about">Quienes Somos</a></span>
-        <span className = "mr-8 text-xl hover:underline hover:text-darkgreen"><a href="/CIBRUC_PW/QuieroSER">Quiero Ser un Super SER</a></span>
-        <span className = "mr-8 text-xl hover:underline hover:text-darkgreen"><a href="/CIBRUC_PW/Contacto">Contacto</a></span>
+        <span className = "mr-8 text-xl hover:underline hover:text-darkgreen"><a href="#quienesSomos">Quienes Somos</a></span>
+        <span className = "mr-8 text-xl hover:underline hover:text-darkgreen"><a href="#ser">Quiero Ser un Super SER</a></span>
+        <span className = "mr-8 text-xl hover:underline hover:text-darkgreen"><a href="#contacto">Contacto</a></span>
       </div>
     </div>
   )
