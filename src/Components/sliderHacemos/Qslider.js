b77031949c385245439bbfa23c9269be72a338e1
Pablo Garcia <pgarciag566@gmail.com>
2022-09-27 22:27:23 -0500

    added a slider for li list instead of entire list

---
+++ src/Components/sliderHacemos/Qslider.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react'
+import QSliderImages from './QsliderData'
+const images = [QSliderImages.uno, QSliderImages.dos, QSliderImages.tres, QSliderImages.tres]
+
+
+export default function Qslider() {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            if(currentIndex === images.length - 1) {
+                setCurrentIndex(0);
+            } 
+            else {
+                setCurrentIndex(currentIndex + 1);
+            }
+        }, 8000)
+        
+        return () => clearInterval(intervalId);
+    }, [currentIndex])
+
+    return (
+        <div>
+            <img className="flex mt-10 border w-96 rounded-xl" src={images[currentIndex]} width="600" height="600" />
+        </div>
+    )
+}
+
---
+++ src/Components/sliderHacemos/QsliderData.js
@@ -0,0 +1,8 @@
+const QSliderImages = {
+    uno: require("../Images/QueHacemosC/uno.png"),
+    dos: require("../Images/QueHacemosC/dos.png"),
+    tres: require("../Images/QueHacemosC/tres.png"),
+    cuatro: require("../Images/QueHacemosC/cuatro.png")
+}
+
+export default QSliderImages
