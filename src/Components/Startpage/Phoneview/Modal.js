bf30bba2cc9a545f23c6ef21dca0fa43992cdb37
Pablo Garcia <pgarciag566@gmail.com>
2022-09-28 01:00:42 -0500

    chaanged padding

--- src/Components/Startpage/Phoneview/Modal.js
+++ src/Components/Startpage/Phoneview/Modal.js
@@ -10,10 +10,10 @@ export default function Modal({ closeModal }) {
             <img src={IMAGES.goBack} width="25px" height="25px" alt=""></img>
         </button>
 
-        <div className = "grid p-4 m-5 rounded-lg justify-items-center mt-14">
-            <span className = "w-full p-4 text-xl text-center rounded-full text-background bg-verydarkgreen"><a href="#quienesSomos"> Quienes Somos</a></span>
-            <span className = "w-full p-4 mt-4 text-xl text-center rounded-full text-background bg-verydarkgreen"><a href="#ser"> Quiero Ser un Super SER</a></span>
-            <span className = "w-full p-4 mt-4 text-xl text-center rounded-full text-background bg-verydarkgreen"><a href="#contacto"> Contacto</a></span>
+        <div className = "grid rounded-lg justify-items-center mt-14">
+            <span className = "w-1/2 py-4 text-xl text-center rounded-full text-background bg-verydarkgreen"><a href="#quienesSomos"> Quienes Somos</a></span>
+            <span className = "w-1/2 py-4 mt-4 text-xl text-center rounded-full text-background bg-verydarkgreen"><a href="#ser"> Quiero Ser un Super SER</a></span>
+            <span className = "w-1/2 py-4 mt-4 text-xl text-center rounded-full text-background bg-verydarkgreen"><a href="#contacto"> Contacto</a></span>
         </div> 
 
 
